fa9cd2c3ec0e374bfb527ebe0ef9d28211aa8daf
hossam-khalaf <hossamhassan303089@gmail.com>
2021-04-29 05:50:45 +0200

    all done

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
-const path = require('path')
+const currentTask = process.env.npm_lifecycle_event
 
-module.exports = {
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
+
+const config = {
 	entry: './app/app.js',
 	output: {
-		filename: 'myBundle.js',
+		filename: 'myBundle.[hash].js',
 		path: path.resolve(__dirname, 'dist'),
 	},
+	plugins: [new HtmlWebpackPlugin({ template: './app/index.html' })],
 
 	module: {
 		rules: [
@@ -15,7 +25,13 @@ module.exports = {
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react'],
+						presets: [
+							[
+								'@babel/preset-env',
+								{ useBuiltIns: 'usage', corejs: 3, targets: 'defaults' },
+							],
+							'@babel/preset-react',
+						],
 					},
 				},
 			},
@@ -33,5 +49,18 @@ module.exports = {
 		// make it super fast by injecting new code not reloading
 		hot: true,
 	},
+	devtool: 'eval-cheap-source-map',
 	mode: 'development',
 }
+
+if (currentTask == 'build') {
+	config.mode = 'production'
+	config.module.rules[1].use[0] = MiniCssExtractPlugin.loader
+	config.plugins.push(
+		new MiniCssExtractPlugin({ filename: 'main.[hash].css' }),
+		new CleanWebpackPlugin(),
+		new WebpackManifestPlugin()
+	)
+}
+
+module.exports = config
